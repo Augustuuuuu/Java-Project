4c490d8eb49b0cbdbe9fe46f378a5d6e186244cd
Augusto Saboia <augustosaboiaestudos@gmail.com>
2024-11-13 15:57:47 -0300

    Average

---
+++ BeerCrowd Java/AverageOne.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class AverageOne {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        float a = read.nextFloat();
+        float b = read.nextFloat();
+        float media = ((a * 3.5f) + (b * 7.5f)) / 11;
+        System.out.printf("MEDIA = %.5f\n", media);
+    }
+}
