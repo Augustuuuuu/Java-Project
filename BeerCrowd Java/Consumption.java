2b4f58b38700b55568575117e06d8cb54472f142
Augusto Saboia <augustosaboiaestudos@gmail.com>
2024-11-14 18:09:29 -0300

    Day 2

---
+++ BeerCrowd Java/Consumption.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Consumption {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int totalDistance = read.nextInt();
+        float totalFuel =  read.nextFloat();
+
+        // The average consumption
+        System.out.printf("%.3f km/l\n",totalDistance /totalFuel);
+    }
+}
